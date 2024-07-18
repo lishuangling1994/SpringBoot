b12cd369ddf9531fb6cdf4e82f6244592b914ebd
lishuangling1994 <997802057@qq.com>
2024-07-18 11:56:20 +0800

    增加mvc的配置类

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -1,4 +1,23 @@
 package com.example.demo.config;
 
-public class WebConfig {
+import com.example.demo.Filter.LoginFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+/*这是一个配置类，用于实现mvc的配置*/
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+    /*拦截器配置*/
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //将自己的拦截器注册到系统拦截器中
+       InterceptorRegistration registration= registry.addInterceptor(new LoginFilter());
+       //配置自己的拦截器路径，所有的路径都拦截
+       registration.addPathPatterns("/**");
+       //配置不拦截的路径,首页和登录请求不拦截,所有的html不拦截
+       registration.excludePathPatterns("index.html","/userLogin/*");
+    }
 }
