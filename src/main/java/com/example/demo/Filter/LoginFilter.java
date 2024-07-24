90a70d2c7676ff3042666d8a4f5a962499b005c2
lishuangling1994 <997802057@qq.com>
2024-07-24 17:23:24 +0800

    里面加了请求uri输出

--- src/main/java/com/example/demo/Filter/LoginFilter.java
+++ src/main/java/com/example/demo/Filter/LoginFilter.java
@@ -1,6 +1,8 @@
 package com.example.demo.Filter;
 
-import lombok.extern.slf4j.Slf4j;
+import com.example.demo.controller.UserLogin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,12 +10,15 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 /*登录拦截器*/
-@Slf4j
 @Component
 public class LoginFilter implements HandlerInterceptor {
+    private static final Logger log= LoggerFactory.getLogger(UserLogin.class);
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        log.info("进入登录过虑器了！");
+        log.info("进入登录过虑器了！地址是"+request.getRequestURI());
+        log.info("请求方法是"+request.getMethod());
+
+
         /*进入登录首页的拦截放过*/
         String uri=request.getRequestURI();
         if(uri.contains("index")){
@@ -42,4 +47,5 @@ public class LoginFilter implements HandlerInterceptor {
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 
     }
+
 }
