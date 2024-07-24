ff7c7b0609dde3554746d14553a29594095dc748
lishuangling1994 <997802057@qq.com>
2024-07-24 17:15:23 +0800

    增加一些工具类

--- src/main/java/com/example/demo/Tool/SequenceUntis.java
+++ src/main/java/com/example/demo/Tool/SequenceUntis.java
@@ -1,8 +1,18 @@
 package com.example.demo.Tool;
 
+
+import java.util.Random;
+
 /**
  * @Auror : lsl
  * @Date: 2024-07-23 16:45
+ * desc : 获取流水号
  */
 public class SequenceUntis {
+    public final static String getSeqSeqNo(){
+        long times=System.currentTimeMillis();
+        Random random=new Random();
+        int r=random.nextInt(1000);
+        return String.format("%d%05d",times,r);
+    }
 }
